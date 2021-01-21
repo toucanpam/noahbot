0ddc3baff5aba290d5f9754336aafd744425f0ef
ZephDaCodes <74450862+ZephDaCodes@users.noreply.github.com>
2021-01-21 22:52:11 +0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,14 +1,20 @@
 module.exports = {
     name:'kick',
-    description: 'kicks a dumbass from the server',
+    description: 'kick people from your life',
     execute(client, message, args){
-        const target = message.mentions.users.first();
-        if(target){
-            const memberTarget = message.guild.members.cache.get(member.id);
-            memberTarget.kick();
-            message.channel.send(`Bye bye, ${user.tag}!`)
+        const {member, mentions} = message
+        
+        if(member.hasPermission('KICK_MEMBERS')){
+            const target = message.mentions.users.first();
+            if(target){
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberYarget.kick();
+                message.channel.send(`${user.tag} just got the yeet`)
+            }else{
+                message.channel.send(`<@${member.id}> who am I going to kick? dumb`)
+            }
         }else{
-            message.channel.send('maybe give me an actual user? smh');
+            message.channel.send(`<@${member.id} you don't even have the perms! lol!`)
         }
     }
 }
